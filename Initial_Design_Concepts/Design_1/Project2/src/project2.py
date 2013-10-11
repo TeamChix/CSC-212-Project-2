6515ed5ad3903c1758ef03618d7223df503bb6c4
cyndijo50 <cryanjo@users.noreply.github.com>
2013-10-11 11:39:28 -0400

    Updated comments - cleaned up code

--- Initial_Design_Concepts/Design_1/Project2/src/project2.py
+++ Initial_Design_Concepts/Design_1/Project2/src/project2.py
@@ -1,15 +1,10 @@
-# To change this template, choose Tools | Templates
-# and open the template in the editor.
+# 
+# CSC 412 Fall 2013 - Project 2
+# implement a prototype for a solution to project 1
+# Selected Scenario - Housing Lottwery
+# Selected Solution - Provide room availablity preview prior to room selection time
+#                     
 import web
-#import ConfigParser
-#import sys
-##from pymongo import MongoClient, errors
-#import json
-#from bson import json_util, BSON
-#import datetime
-#import dateutil.parser
-#from collections import defaultdict
-#import os
 
 __author__="US978847"
 __date__ ="$Oct 3, 2013 12:47:14 PM$"
@@ -22,7 +17,6 @@ class Index(object):
 
 urls = (
   '/', Index
-  #'/modelquery', ModelQuery
 );
 
 if __name__ == "__main__":
