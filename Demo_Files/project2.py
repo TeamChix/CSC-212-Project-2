664b7e4823ae6f417a7490af0e882b9b776f5f04
cyndijo50 <cryanjo@users.noreply.github.com>
2013-10-14 09:12:33 -0400

    Commented and cleaned up

--- Demo_Files/project2.py
+++ Demo_Files/project2.py
@@ -1,28 +1,20 @@
-# To change this template, choose Tools | Templates
-# and open the template in the editor.
+# 
+# start up a local server for project 2 demonstration
+# need a python environment and web.py installed to run - http://webpy.org/
+#
 import web
-#import ConfigParser
-#import sys
-##from pymongo import MongoClient, errors
-#import json
-#from bson import json_util, BSON
-#import datetime
-#import dateutil.parser
-#from collections import defaultdict
-#import os
 
-__author__="US978847"
+__author__="CRyan"
 __date__ ="$Oct 3, 2013 12:47:14 PM$"
 
 # Redirect to index.html file
 class Index(object):
   def GET(self):
-    raise web.seeother('../index.html');
+    raise web.seeother('../static/index.html');
 
 
 urls = (
   '/', Index
-  #'/modelquery', ModelQuery
 );
 
 if __name__ == "__main__":
